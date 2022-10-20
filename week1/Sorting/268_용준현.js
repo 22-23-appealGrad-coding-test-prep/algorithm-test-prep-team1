09f131437bf7b7d1154bf22977ccd48b1f5b8cde
pione3r <jhyong0626@gmail.com>
2022-10-20 17:49:06 +0900

    [용준현/문제해결] 1주차 Sorting 문제 해결 / 정렬 메소드 사용 풀이 추가

--- week1/Sorting/268_용준현.js
+++ week1/Sorting/268_용준현.js
@@ -1,4 +1,5 @@
 /**
+ * 등차 합 공식 사용해서 수학적으로 풀이
  * @param {number[]} nums
  * @return {number}
  */
@@ -11,4 +12,16 @@
   }
 
   return sum1 - sum2;
+};
+
+/**
+ * 배열 정렬 메소드를 사용해서 풀이
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var missingNumber = function (nums) {
+  let sortedNums = nums.sort((a, b) => a - b);
+  for (let i = 0; i <= nums.length; i++) {
+      if (sortedNums[i] !== i) return i;
+  }
 };
