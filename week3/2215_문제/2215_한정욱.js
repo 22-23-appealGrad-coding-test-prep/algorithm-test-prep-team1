576da02eb2e12b92a2539bd950a830bdc33aaa58
robin <nowrobin3@gmail.com>
2022-11-06 18:42:33 +0900

    [한정욱/문제해결] 2215번 문제풀이완료

--- week3/2215_문제/2215_한정욱.js
+++ week3/2215_문제/2215_한정욱.js
@@ -5,37 +5,39 @@
  */
  var findDifference = function(nums1, nums2) {
     
-    let n1 = nums1.length;
-    let n2 = nums2.length;
-    
-    let temp = []
-    let result = [[],[]]
-    for(let i = 0; i<n1; i++){
-        for(let j=0; j<n2; j++){
-            if(nums1[i]===nums2[j]){
-                temp.push(nums1[i]);
-            }
-        }
-    }   
-    let k =0; 
-    let p =0;
-    for(let i = 0; i<n1; i++){
-        if((!temp.includes(nums1[i]))&&(!result[0].includes(nums1[i]))&& !undefined ){
-            
-            result[0][k++] = nums1[i];
-            
-        }
-        if((!temp.includes(nums2[i]))&&(!result[1].includes(nums2[i]))&& !undefined){
-            result[1][p++] = nums2[i];
-        }
-    }
-    
-    console.log(result)
 
-    return result
+    let l1 = new Set(nums1);
+    let l2 = new Set(nums2);
+    let diff1 = new Set([...l1].filter(num=> !l2.has(num)))
+    let diff2 = new Set([...l2].filter(num=> !l1.has(num)))
+    
+    let result = [[...diff1],[...diff2]];
+    return result;
 
+    // let n1 = nums1.length;
+    // let n2 = nums2.length;
+    // let temp = []
+    // for(let i = 0; i<n1; i++){
+    //     for(let j=0; j<n2; j++){
+    //         if(nums1[i]===nums2[j]){
+    //             temp.push(nums1[i]);
+    //         }
+    //     }
+    // }   
+    // let k =0; 
+    // let p =0;
+    // for(let i = 0; i<n1; i++){
+    //     if((!temp.includes(nums1[i]))&&(!result[0].includes(nums1[i]))&& !undefined ){
+            
+    //         result[0][k++] = nums1[i];
+            
+    //     }
+    //     if((!temp.includes(nums2[i]))&&(!result[1].includes(nums2[i]))&& !undefined){
+    //         result[1][p++] = nums2[i];
+    //     }
+    // }
+    // return result
 };
-
 
 
 
