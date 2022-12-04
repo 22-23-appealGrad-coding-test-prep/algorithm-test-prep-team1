d2fcf789c74efd2f1b6f9f11865e6fe7ab3dc9de
robin <nowrobin3@gmail.com>
2022-12-04 20:46:24 +0900

    [한정욱/문제풀이] 2번문제풀이 다시 제출

--- week6/2번_문제/2_한정욱.js
+++ week6/2번_문제/2_한정욱.js
@@ -42,7 +42,39 @@
             l1.val = l2;
              if(l1.val>9){
             l1.val = l1.val -10;
-            l1 = l1.next;
+            l1 = l1.next;/**
+            * Definition for singly-linked list.
+            * function ListNode(val, next) {
+            *     this.val = (val===undefined ? 0 : val)
+            *     this.next = (next===undefined ? null : next)
+            * }
+            */
+           /**
+            * @param {ListNode} l1
+            * @param {ListNode} l2
+            * @return {ListNode}
+            */
+           var addTwoNumbers = function(l1, l2) { 
+               let newList = new ListNode(0);
+               let head = newList;
+               let carry = 0; 
+           
+               while(l1 || l2 ){
+                   const sum =(l1 ? l1.val :  0) + (l2 ? l2.val : 0) + carry;
+                   //l1 l2 존재하는지 확인하고  캐리를 더함
+                   newList.next = new ListNode(sum >= 10 ?  sum-10 : sum );
+                   carry = sum >= 10 ? 1 : 0;
+                   //carry = sum >= 10 ? Math.floor(sum/10) : 0;
+                   l1 = l1 ? l1.next : null;
+                   l2 = l2 ? l2.next : null;
+                   newList = newList.next;
+               }
+           
+               if(carry> 0){
+                   newList.next = new ListNode(carry);
+               }
+               return head.next;
+           };
             l1.val = l1.val +1;
             }   
             l1 = l1.next;
