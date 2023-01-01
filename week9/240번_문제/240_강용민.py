a644c42faa93bab172d8cfdad051935f9f35a5e1
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-01 20:10:08 +0900

    [강용민/문제해결] 240번 문제풀이 완료

---
+++ week9/240번_문제/240_강용민.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for row in matrix:
+            if target in row:
+                return True
+        return False
